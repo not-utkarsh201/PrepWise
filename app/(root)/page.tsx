f60e1c519db7b369b036bdf5702b091ffb440a47
not-utkarsh201 <work.utkarsh201@gmail.com>
2025-04-01 12:55:44 +0530

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -86,6 +86,10 @@ async function Home() {
             <p>There are no interviews available</p>
           )}
         </div>
+        <div>
+          Created By Utkarsh
+          <a href="https://github.com/not-utkarsh201/PrepWise">Github</a>
+        </div>
       </section>
     </>
   );
